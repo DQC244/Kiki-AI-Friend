6308f7445b6fb1bf4ed372cf7928dbcd4219f9d6
Lucky <long.nguyenvu@ascendcorp.com>
2023-04-13 13:20:29 +0700

    [i18n] Add i18n for header, footer and birth-chart

--- src/language/resources/vn/common.lang.ts
+++ src/language/resources/vn/common.lang.ts
@@ -2,5 +2,29 @@ export default {
   lKeyLang: "VI",
   lLogin: "Login",
   lComingSoonQ3: "Sắp ra mắt vào Quý 3 năm 2023",
-  lFollowUsOn: "Follow us on",
+  lFollowUsOn: "Theo dõi bọn mình trên",
+  lTermsConditions: "Điều khoản & Điều kiện",
+  lAllRightsReserved: "All Rights Reserved.",
+
+  // Chart
+  lCreateYourOwnBirth: "Tạo Bản đồ sao miễn phí của riêng bạn!",
+  lTellUsALittle: "Hãy cho chúng tôi biết đôi điều về bạn nhé!",
+  lMyNameIs: "Tên tôi là:",
+  lIWasBornOn: "Tôi sinh ra vào:",
+  lAt: "lúc",
+  lIn: "Tại:",
+  lCreateChart: "Tạo bản đồ sao",
+
+  ObjMenu: {
+    lHome: "Trang Chủ",
+    lChart: "Bản đồ sao",
+    lTarot: "Tarot",
+    lDownload: "Tải ứng dụng",
+    lShop: "Cửa hàng",
+    lBirthChart: "Birth Chart",
+    lTransitChart: "Transit Chart",
+    lSynastryChart: "Synastry Chart",
+    lDailyTarot: "Daily Tarot",
+    lTarotCardMeaning: "Tarot Card Meaning",
+  },
 };
