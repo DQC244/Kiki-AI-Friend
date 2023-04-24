75d7237d74b81af7d781e8822481f05db7e6a49b
Lucky <long.nguyenvu@ascendcorp.com>
2023-04-24 16:11:16 +0700

    [i18n] Add i18n for home, download and shop

--- src/language/resources/vn/common.lang.ts
+++ src/language/resources/vn/common.lang.ts
@@ -5,6 +5,47 @@ export default {
   lFollowUsOn: "Theo dõi bọn mình trên",
   lTermsConditions: "Điều khoản & Điều kiện",
   lAllRightsReserved: "All Rights Reserved.",
+  lAcceptAll: "Accept All",
+  lPreferences: "Preferences",
+  lRejectAll: "Reject All",
+  lHaveACookie: "Have a cookie",
+  lCookiesDesc:
+      "We use cookies to ensure that we give you the best experience on our website. We also use cookies to ensure we show you advertising that is relevant to you. You can change your cookie settings at anytime.",
+  lAllowAll: "Allow All",
+  lOnlyNecessary: "Only Necessary",
+  lOnlyNecessaryDesc:
+      "here cookies are strictly necessary to provide services that you have requested.",
+  lFunctionalityCookies: "Functionality Cookies",
+  lFunctionalityCookiesDesc:
+      "These cookies allow us to remember choices you make (such as your user name, language or the region you are in) and provide enhanced, more relevant features.",
+  lAdvertisingCookies: "Advertising cookies",
+  lAdvertisingCookiesDesc:
+      "Advertisement cookies help us provide our visitors with relevant ads and marketing campaigns.",
+  lPerformanceCookies: "Performance Cookies",
+  lPerformanceCookiesDesc:
+      "These cookies collect information about how visitors use our website. The information collected is anonymous and is only used to improve the way our website works.",
+  lBack: "Back",
+  lFinish: "Finish",
+
+  // Home
+  lWelcomeToTheFantasyWorld:
+      "Chào mừng đến với xứ sở diệu kỳ - nơi bạn được là chính mình và được hỗ trợ về mặt tâm linh và sức khỏe tinh thần.",
+  lAIFriend: "Người bạn AI",
+  lAIFriendDesc:
+      "Là người bạn của riêng bạn, Kiki luôn lắng nghe và đưa ra những báo cáo chi tiết nhất để cuộc sống của bạn dễ dàng hơn.<br/>Kiki luôn có mặt bất cứ khi nào, quan tâm đến bạn từ lúc thức dậy tới khi bạn say giấc. Kiki - một người bạn đồng hành tuyệt vời, người đưa ra cho bạn những giải pháp thiết thực, cái nhìn lạc quan, hay chỉ đơn giản là cùng trò chuyện với bạn.",
+  lWannaSayHello: "Bạn muốn ghé qua chào Kiki chứ?",
+  lMysticZone: "Vùng đất huyền bí",
+  lMysticZoneDesc:
+      "Nơi bạn tìm thấy câu trả lời cho những khó khăn đang gặp phải. Nếu bạn đang băn khoăn, hãy nhờ vũ trụ giải đáp.",
+  lWantToKnowWhatTheUniverse: "Muốn biết vũ trụ đang có gì muốn nhắn nhủ với bạn? Thử ngay thôi!",
+  lSeeHarmony: "Xem mức độ hòa hợp",
+  lSeeHarmonyDesc:
+      "Thắc mắc liệu bạn và người ấy có phải là hai mảnh ghép hoàn hảo? Hay đơn giản chỉ là bạn có hợp với ai đó hay không?",
+  lLetSeeYourHarmonyWith: "Cùng xem độ hòa hợp của bạn và người ấy thôi!",
+  lGenerateMyBirthChart: "Tạo bản đồ sao của tôi",
+  lIWasBornIn: "Tôi sinh ra tại:",
+  lOn: "vào ngày",
+  lCity: "thành phố",
 
   // Chart
   lCreateYourOwnBirth: "Tạo Bản đồ sao miễn phí của riêng bạn!",
@@ -14,6 +55,99 @@ export default {
   lAt: "lúc",
   lIn: "Tại:",
   lCreateChart: "Tạo bản đồ sao",
+  lHeyIAmHere: "Hey, I’m here. Scroll down to explore more.",
+  lWantToTryHave: "Want to try having a short conversation with Kiki?",
+  lPleaseChooseTheTopic: "Please choose the topic that concerns you most right now!",
+  lPleaseChooseQuestion: "Great! Now choose a question you want to ask!",
+  lYouCanClickOnTheArrow: "You can click on the arrow button to hide or show the topics panel.",
+  lYourCurrentLocation: "Your current location:",
+  lLookAheadInto: "Look ahead into the coming days, weeks and years with Transit chart",
+  lCalculateTransit: "Calculate Transit",
+  lBirth: "Birth",
+  lTransit: "Transit",
+  lLocalTime: "Local time:",
+  lUniversalTime: "Universal time:",
+  lHouseSystem: "House system:",
+  lCityCountry: "City, Country:",
+  lName: "Name:",
+  lDateOfBirth: "Date of birth:",
+  lPlaceOfBirth: "Place of birth:",
+  lFindOutWho: "Find out who matches your spiritual energy",
+  lViewSynastryChart: "View Synastry Chart",
+  lLetSeeYourHarmony: "Let’s see your harmony and how well you two get on. Try our app now!",
+  lPleaseEnterAllRequiredFields: "Please enter all required fields",
+  lThinkAboutTheQuestionYouAsk: "Think about the question you wanna ask.",
+  lWhenYouAreDone:
+      "When you’re done, click on the button below and I’ll get your answer from the universe.",
+  lImReady: "I’m ready",
+  lThinkAboutAnotherPossibility: "Think about another possibility",
+  lJustADayDream: "It’s just a daydream! Wake up honey.",
+  lJustGoIt: "Just go for it!",
+  lItNotRightTime: "It’s not the right time.",
+  lKeepThinkingAboutAnother: "Keep thinking about another possibility you wanna ask me!",
+  lPingMeAgainWhenYouAreReady: "Ping me again when you’re ready!",
+  lJustComeBackAnytimeWith:
+      "Just come back anytime with a fish or download our app to explore more and receive many more messages from the universe.",
+  lDownloadOurAppIfYouWant: "Download our app if you want to have more conversation with me!",
+  lYouCanThinkAboutAnotherPossibility:
+      "You can think about another possibility or slide the topic bar to ask another question.",
+  lGreatClickOnTheButton: "Great! Click on the button if you’re ready!",
+  lInsteadDoYouWantToTryOut:
+      "Instead, do you want to try out the Mystic Zone? Download our app to explore more!",
+
+  // Daily tarot, meaning
+  lOneCardTarotReading: "Trải bài một lá",
+  lGetYourDailyMessage:
+      "Nhận thông điệp hàng ngày từ vũ trụ để có thể tận hưởng một ngày thật trọn vẹn.",
+  lGetMyReading: "Xem trải bài",
+  lRedrawACard: "Rút lại trải bài",
+  lDownloadOurAppToTryReading: "Hãy tải ứng dụng để trải nghiệm trải bài hai lá!",
+  lTarotCardMeaning: "Ý nghĩa lá bài",
+  lViewCards: "Xem lá bài",
+  lReadTheMeaning: "Xem ý nghĩa",
+  lKeyMeaning: "Key meaning:",
+  lSign: "Sign",
+  lPlanet: "Planet",
+  lElement: "Element",
+  lAquarius: "Aquarius",
+  lUranus: "Uranus",
+  lAir: "Air",
+  lTheFoolOverview: "The Fool Overview:",
+  lMessageFromTheFool: "Message from The Fool:",
+  lPickTodayTarotCard: "Pick Today's Tarot Card",
+  lOrderNow: "Order Now",
+
+  // Download
+  lComingSoonAppStoreAndGooglePlay: "Sắp ra mắt trên Apple Store và Google Play vào Quý 3 năm 2023",
+  lDoYouWantToBeTheFirstPerson:
+      "Bạn có muốn trở thành người đầu tiên trải nghiệm ứng dụng của chúng tôi? Hãy để lại email phía bên dưới và chúng tôi sẽ gửi thông báo đến bạn khi ứng dụng được ra mắt!",
+  lYourMail: "Email của bạn",
+
+  // Shop
+  lDescription: "Mô tả:",
+  lBuyOnTikTokShop: "Mua trên TikTok Shop",
+  lInquireViaInstagram: "Đặt hàng qua Instagram",
+  lJoinOurNewsLetters: "Tham gia bản tin của chúng tôi",
+  lDoYouWantFirstPersonToExperience:
+      "Bạn muốn là một trong những người đầu tiên trải nghiệm ứng dụng và cầm trên tay các sản phẩm mới nhất của Kiki chứ? Hãy để lại email bên dưới và chúng tôi sẽ thông báo cho bạn khi có thông tin mới!",
+
+  // No EN
+  lImSorryITryImprove: "Tôi xin lỗi, tôi sẽ cố gắng cải thiện hơn trong tương lai.",
+
+  objChatContentDefault: {
+    lHiThere: "Hi there 👋",
+    lMyNameIsKiki: "My name is Kiki",
+    lWhatBringYouHereToday: "What bring you here today?",
+  },
+
+  objQuestionStep1: {
+    lLove: "Love",
+    lMoney: "Money",
+    lSelf: "Self",
+    lWorkStudy: "Work / Study",
+    lPossibilities: "Possibilities",
+    lNoImGood: "No, I’m Good.",
+  },
 
   ObjMenu: {
     lHome: "Trang Chủ",
@@ -26,5 +160,45 @@ export default {
     lSynastryChart: "Synastry Chart",
     lDailyTarot: "Daily Tarot",
     lTarotCardMeaning: "Tarot Card Meaning",
+  },
+
+  // Chat
+  lIveChangedMyMind: "I’ve changed my mind. Can I change topics?",
+
+  objWorkTopic: {
+    lWhatCareersSuitMeTheMost: "What careers suit me the most?",
+    lAnyNewOpportunity: "Any new opportunity for my job/ study?",
+    lShouldIStart: "Should I start a Start-up?",
+    lShouldIPursue: "Should I pursue a higher education?",
+  },
+
+  objLoveTopic: {
+    lTellMeAbout: "Tell me about my marriage life.",
+    lSignOfMyNextLove: "Sign of my next love/romantic relationship?",
+    lWhatIsMyIdealization: "What is my idealization of love and beauty?",
+  },
+
+  objSelfTopic: {
+    lWhatIsMyNutritionalAdvice: "What is my nutritional advice?",
+    lWhatIsMyStressManagementTips: "What is my stress management tips?",
+    lCanYouTellMyPersonality: "Can you tell my personality?",
+  },
+
+  objMoneyTopic: {
+    lWhereCanIGrow: "Where can I grow or find my good fortune from?",
+  },
+
+  objEndTopic: {
+    lAskAnotherQuestion: "Ask another question in the same topics.",
+    lBackToOtherMainTopics: "Back to other main topics.",
+    lEndConversation: "End Conversation.",
+  },
+
+  objCardName: {
+    lTheMajorArcana: "The Major Arcana",
+    lTheSuitOfCups: "The Suit of Cups",
+    lTheSuitOfWands: "The Suit of Wands",
+    lTheSuitOfSwords: "The Suit of Swords",
+    lTheSuitOfPentacles: "The Suit of Pentacles",
   },
 };
